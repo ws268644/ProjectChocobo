88df12ee78dc68e5f74e87629f3350b6afbcfa56
l4-ingram <lewis2.ingram@live.uwe.ac.uk>
2022-01-05 16:34:53 +0000

    Tested database connection and got it working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,14 @@ namespace ProjectChocobo
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- clsDatabase.cs
+++ clsDatabase.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace ProjectChocobo
 {
     static class clsDatabase
     {
-        static string conString = "server=ws265660.remote.ac;user = WS265660_Admin;database = WS265660_ProjectChocobo;password =98U*z4rl;CharSet=utf8;SslMode=none;";
+        static string conString = "server=ws268644.remote.ac;user = ws268644_Admin;database = ws268644_ProjectChocobo;password =98U*z4rl;CharSet=utf8;SslMode=none;";
 
     }
 }
