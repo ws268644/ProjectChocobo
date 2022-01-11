592995f6956a8256bb0e2153da1147f82ea7216e
Lewis Ingram <44513246+Auragen-dev@users.noreply.github.com>
2022-01-11 15:04:29 +0000

    Revert "Completed login(). Now to test it"
    This reverts commit 795374689c5d341a27b3356d2275fbc5d9e32da8.

--- clsDatabase.cs
+++ clsDatabase.cs
@@ -14,34 +14,11 @@ namespace ProjectChocobo
     {
         static private string conString = "server=ws268644.remote.ac;user = ws268644_Admin;database = ws268644_ProjectChocobo;password =98U*z4rl;CharSet=utf8;SslMode=none;";
 
-        static public Boolean login(string strUser, string strPass) {
+        static public void login(string strUser, string strPass) {
             MySqlConnection cnn = new MySqlConnection(conString); //Sets connection string as an actual SQL connection
-            MySqlCommand comLogin = new MySqlCommand("checkLogin", cnn);
-            MySqlCommand comPass = new MySqlCommand("checkPassword", cnn);
-            comLogin.CommandType = System.Data.CommandType.StoredProcedure; //Tells C# to treat the command as a stored procedure
-            comLogin.Parameters.AddWithValue("@loginUsername", strUser);
-            comPass.CommandType = System.Data.CommandType.StoredProcedure;
-            comPass.Parameters.AddWithValue("@loginPassword", strPass);
-            try
-            {
-                cnn.Open();
-                int loginCheck = comLogin.ExecuteNonQuery();
-                if (loginCheck == -1) { 
-                    int passCheck = comPass.ExecuteNonQuery();
-                    cnn.Close();
-                    if (passCheck == -1) {
-                        return true;
-                        
-                    }
-                }
-                cnn.Close();
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("There was an error:\n"+ex);
-            }
-            return false;
+            MySqlCommand mycom = new MySqlCommand("checkLogin", cnn);
+            mycom.CommandType = System.Data.CommandType.StoredProcedure; //Tells C# to treat the command as a stored procedure
+
 
         }
     }
