75f89cd0ea8d38e524eb98b7bcfcc8d8088e2633
Lewis Ingram <44513246+Auragen-dev@users.noreply.github.com>
2022-01-11 13:43:56 +0000

    Started login function. Defined a database connection and command. Set command as a stored procedure

--- clsDatabase.cs
+++ clsDatabase.cs
@@ -12,7 +12,14 @@ namespace ProjectChocobo
 {
     static class clsDatabase
     {
-        static string conString = "server=ws268644.remote.ac;user = ws268644_Admin;database = ws268644_ProjectChocobo;password =98U*z4rl;CharSet=utf8;SslMode=none;";
+        static private string conString = "server=ws268644.remote.ac;user = ws268644_Admin;database = ws268644_ProjectChocobo;password =98U*z4rl;CharSet=utf8;SslMode=none;";
 
+        static public void login(string strUser, string strPass) {
+            MySqlConnection cnn = new MySqlConnection(conString); //Sets connection string as an actual SQL connection
+            MySqlCommand mycom = new MySqlCommand("checkLogin", cnn);
+            mycom.CommandType = System.Data.CommandType.StoredProcedure; //Tells C# to treat the command as a stored procedure
+
+
+        }
     }
 }
