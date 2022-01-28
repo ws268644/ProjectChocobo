f279a0d2f1c0b53fe855b8898d3712a8d4d4de3e
l4-ingram <lewis2.ingram@live.uwe.ac.uk>
2022-01-28 02:35:04 +0000

    Wrote setup code and the getUserID code

--- clsDatabase.cs
+++ clsDatabase.cs
@@ -16,10 +16,30 @@ namespace ProjectChocobo
 
         static public void login(string strUser, string strPass) {
             MySqlConnection cnn = new MySqlConnection(conString); //Sets connection string as an actual SQL connection
-            MySqlCommand mycom = new MySqlCommand("checkLogin", cnn);
+            MySqlCommand mycom = new MySqlCommand("@checkLogin", cnn);
             mycom.CommandType = System.Data.CommandType.StoredProcedure; //Tells C# to treat the command as a stored procedure
 
 
+        }
+        static public void applyUserRole(string strUsername) {
+            int intUserID = 0;
+            MySqlConnection cnn = new MySqlConnection(conString); //Sets connection string as an actual SQL connection
+            MySqlCommand comApplyUserRole = new MySqlCommand("applyUserRole", cnn);
+            MySqlCommand comGetID = new MySqlCommand("getUserID", cnn);
+            comApplyUserRole.CommandType = System.Data.CommandType.StoredProcedure; //Tells C# to treat the command as a stored procedure
+            comGetID.CommandType = System.Data.CommandType.StoredProcedure;
+            comGetID.Parameters.AddWithValue("@username", strUsername);
+            comApplyUserRole.Parameters.AddWithValue("@userID", intUserID);
+            try
+            {
+                cnn.Open();
+                intUserID = Convert.ToInt32(comGetID.ExecuteScalar());
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
