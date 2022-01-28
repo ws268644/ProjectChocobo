6681c09be0f7d9cd70f528bdda501dd786c5ff33
l4-ingram <lewis2.ingram@live.uwe.ac.uk>
2022-01-28 02:21:13 +0000

    Added a function which will create new users. Returns a boolean depending on success or failure

--- clsDatabase.cs
+++ clsDatabase.cs
@@ -21,5 +21,45 @@ namespace ProjectChocobo
 
 
         }
+        static public Boolean createNewUser(String strUsername, String strPassword)
+        {
+            MySqlConnection cnn = new MySqlConnection(conString); //Sets connection string as an actual SQL connection
+            MySqlCommand comCreateUser = new MySqlCommand("createUser", cnn);
+            MySqlCommand comCheckUser = new MySqlCommand("usernameTakenCheck", cnn);
+            comCreateUser.CommandType = System.Data.CommandType.StoredProcedure; //Tells C# to treat the command as a stored procedure
+            comCheckUser.CommandType = System.Data.CommandType.StoredProcedure;
+            comCreateUser.Parameters.AddWithValue("@username", strUsername);
+            comCreateUser.Parameters.AddWithValue("@username", strPassword);
+            comCheckUser.Parameters.AddWithValue("@username", strUsername);
+            try
+            {
+                cnn.Open();
+                int userCheck = Convert.ToInt32(comCheckUser.ExecuteScalar()); //Checks if the users username already exists
+                if (userCheck < 0)
+                {
+                    cnn.Close();
+                    return false;
+                }
+                int intSuccess = Convert.ToInt32(comCreateUser.ExecuteNonQuery()); //Number represented is how many rows were affected. If it returns one, it worked fine.
+                cnn.Close();
+                if (intSuccess == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error:\n" + ex);
+            }
+            return false;
+        }
+
     }
-}
+
+    }
+
