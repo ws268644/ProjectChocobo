2bb7212b5f133071495b4fa70eb9033f932656f4
Lewis Ingram <44513246+Auragen-dev@users.noreply.github.com>
2022-04-05 14:54:10 +0100

    Gave buttons names

--- admin.Designer.cs
+++ admin.Designer.cs
@@ -34,6 +34,10 @@ namespace ProjectChocobo
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btn_logout = new System.Windows.Forms.Button();
             this.btn_quit = new System.Windows.Forms.Button();
+            this.btnTracks = new System.Windows.Forms.Button();
+            this.btnEvents = new System.Windows.Forms.Button();
+            this.btn_settings = new System.Windows.Forms.Button();
+            this.btn_profiles = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -78,14 +82,58 @@ namespace ProjectChocobo
             this.btn_quit.UseVisualStyleBackColor = true;
             this.btn_quit.Click += new System.EventHandler(this.btn_quit_Click);
             // 
+            // btnTracks
+            // 
+            this.btnTracks.Location = new System.Drawing.Point(12, 195);
+            this.btnTracks.Name = "btnTracks";
+            this.btnTracks.Size = new System.Drawing.Size(393, 117);
+            this.btnTracks.TabIndex = 6;
+            this.btnTracks.Text = "Tracks";
+            this.btnTracks.UseVisualStyleBackColor = true;
+            this.btnTracks.Click += new System.EventHandler(this.btnTracks_Click);
+            // 
+            // btnEvents
+            // 
+            this.btnEvents.Location = new System.Drawing.Point(401, 60);
+            this.btnEvents.Name = "btnEvents";
+            this.btnEvents.Size = new System.Drawing.Size(387, 117);
+            this.btnEvents.TabIndex = 5;
+            this.btnEvents.Text = "Events";
+            this.btnEvents.UseVisualStyleBackColor = true;
+            this.btnEvents.Click += new System.EventHandler(this.btnEvents_Click);
+            // 
+            // btn_settings
+            // 
+            this.btn_settings.Location = new System.Drawing.Point(401, 195);
+            this.btn_settings.Name = "btn_settings";
+            this.btn_settings.Size = new System.Drawing.Size(387, 117);
+            this.btn_settings.TabIndex = 25;
+            this.btn_settings.Text = "Settings";
+            this.btn_settings.UseVisualStyleBackColor = true;
+            this.btn_settings.Click += new System.EventHandler(this.btn_settings_Click);
+            // 
+            // btn_profiles
+            // 
+            this.btn_profiles.Location = new System.Drawing.Point(12, 60);
+            this.btn_profiles.Name = "btn_profiles";
+            this.btn_profiles.Size = new System.Drawing.Size(393, 117);
+            this.btn_profiles.TabIndex = 26;
+            this.btn_profiles.Text = "Profiles";
+            this.btn_profiles.UseVisualStyleBackColor = true;
+            this.btn_profiles.Click += new System.EventHandler(this.btn_profiles_Click);
+            // 
             // frmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_profiles);
+            this.Controls.Add(this.btn_settings);
             this.Controls.Add(this.btn_logout);
             this.Controls.Add(this.btn_quit);
             this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.btnTracks);
+            this.Controls.Add(this.btnEvents);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmAdmin";
             this.Text = "Admin page";
@@ -101,5 +149,9 @@ namespace ProjectChocobo
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btn_logout;
         private System.Windows.Forms.Button btn_quit;
+        private System.Windows.Forms.Button btnTracks;
+        private System.Windows.Forms.Button btnEvents;
+        private System.Windows.Forms.Button btn_settings;
+        private System.Windows.Forms.Button btn_profiles;
     }
 }
